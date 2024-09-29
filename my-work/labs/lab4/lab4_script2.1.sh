e421d78fcb5dc219a557a1e06128889f5ca47325
tcusick8 <sidtom8@aol.com>
2024-09-29 13:48:32 -0400

    Update lab4_script2.1.sh

--- my-work/labs/lab4/lab4_script2.1.sh
+++ my-work/labs/lab4/lab4_script2.1.sh
@@ -7,19 +7,15 @@ pip install requests boto3
 import requests
 import boto3
 
-# Download the image
 url = "https://i.guim.co.uk/img/media/3bdec8a12f277b08282e00c8afa24a9c737a4e4f/0_251_6000_3600/master/6000.jpg?width=1200&height=900&quality=85&auto=format&fit=&fit=crop&s=e1ddf9154bc726101f575b705c28c156"
 response = requests.get(url)
 
-# Save the image to a file
 with open("taylor_swift_lab4_image.png", "wb") as file:
     file.write(response.content)
 
-# Upload the image to S3
 s3 = boto3.client("s3")
 s3.upload_file("taylor_swift_lab4_image.png", "ds2022-tpg6hu", "taylor_swift_lab4_image.png")
 
-# Generate a presigned URL
 presigned_url = s3.generate_presigned_url(
     "get_object",
     Params={"Bucket": "ds2022-tpg6hu", "Key": "taylor_swift_lab4_image.png"},
